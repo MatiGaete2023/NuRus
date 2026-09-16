a1855620dedb0d2c6b99766f60a3b034252de2a5
MatiGaete2023 <154522773+MatiGaete2023@users.noreply.github.com>
2026-09-16 13:13:51 -0300

    Load reviewed export safety fix

--- src/nurus/personal/__init__.py
+++ src/nurus/personal/__init__.py
@@ -6,3 +6,4 @@
 from . import runtime_fixes_20260916 as _runtime_fixes_20260916  # noqa: F401,E402
 from . import runtime_fixes_20260916_ui as _runtime_fixes_20260916_ui  # noqa: F401,E402
 from . import runtime_fixes_20260916_projects as _runtime_fixes_20260916_projects  # noqa: F401,E402
+from . import runtime_fixes_20260916_safety as _runtime_fixes_20260916_safety  # noqa: F401,E402
